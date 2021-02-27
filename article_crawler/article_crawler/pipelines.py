eee849af66d82b44e12b8f4ded5c22c1838a983e
Hao Song <haosong.pitt@gmail.com>
2021-02-27 13:16:40 -0600

    Update pipelines.py
    1) fix typo lastUPdated -> lastUpdated
    2) fix bug that does not clean date

--- article_crawler/article_crawler/pipelines.py
+++ article_crawler/article_crawler/pipelines.py
@@ -10,13 +10,13 @@ from datetime import datetime
 
 class CheckItemPipeline:
     def process_item(self, article, spider):
-        if not article['lastUPdated'] or not article['url'] or not article['title']:
+        if not article['lastUpdated'] or not article['url'] or not article['title']:
             raise DropItem('Missing something!')
         return article
 
 
 class CleanDatePipeline:
     def process_item(self, article, spider):
-        article['lastUpdated'].replace('This page was last edited on', '').strip()
+        article['lastUpdated'] = article['lastUpdated'].replace('This page was last edited on', '').strip()
         article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H:%M')
         return article
